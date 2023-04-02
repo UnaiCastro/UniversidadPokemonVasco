0b550c468eb2265f67eb32d70d32cac19937792a
oier_diez <82831105+Foxo10@users.noreply.github.com>
2023-04-03 01:14:14 +0200

    Update ProgramPrincipal.java

--- src/Vista/ProgramPrincipal.java
+++ src/Vista/ProgramPrincipal.java
@@ -1,9 +1,6 @@
 package Vista;
 
 import Modelo.GestorJuegoPokemon;
-import Modelo.Jugador;
-import Vista.InterfazJuegoPokemon;
-import Vista.InterfazJugador;
 
 public class ProgramPrincipal {
 	public static void main(String[] args) {
