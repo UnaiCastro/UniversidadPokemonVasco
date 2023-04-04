0f34dcb3cc716d36d71d0256829fce2df73d0ce9
oier_diez <82831105+Foxo10@users.noreply.github.com>
2023-04-04 12:31:13 +0200

    event evoke later

--- src/Vista/ProgramPrincipal.java
+++ src/Vista/ProgramPrincipal.java
@@ -1,18 +1,17 @@
 package Vista;
 
+import java.awt.EventQueue;
 import Modelo.GestorJuegoPokemon;
-import Modelo.Jugador;
-import Vista.InterfazJuegoPokemon;
-import Vista.InterfazJugador;
 
 public class ProgramPrincipal {
 	public static void main(String[] args) {
 		System.out.println("Bienvenidos A Pokemon");
 		GestorJuegoPokemon.getMiGestorJuegoPokemon();
-		InterfazJuegoPokemon interfaz = new InterfazJuegoPokemon();
-		//String player="Player";
-		//int pokemon= 1;
-		//int jugador=1;
-		//InterfazJugador interfazJugador = new InterfazJugador(player,pokemon,jugador);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				InterfazJuegoPokemon interfaz = new InterfazJuegoPokemon();
+			}
+		});
+		
 	}
 }
